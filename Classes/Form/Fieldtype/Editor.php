0a68fdfdec25fcf9e5042eed3af5993e27a51bcf
Bastian Zagar <zagar@aixdesign.net>
2017-05-16 19:16:45 +0200

    New Editor Field

---
+++ Classes/Form/Fieldtype/Editor.php
@@ -0,0 +1,78 @@
+<?php
+namespace MageDeveloper\Dataviewer\Form\Fieldtype;
+
+use MageDeveloper\Dataviewer\Domain\Model\Field;
+use MageDeveloper\Dataviewer\Domain\Model\RecordValue;
+use TYPO3\CMS\Backend\Form\Container\SingleFieldContainer;
+
+/**
+ * MageDeveloper Dataviewer Extension
+ * -----------------------------------
+ *
+ * @category    TYPO3 Extension
+ * @package     MageDeveloper\Dataviewer
+ * @author		Bastian Zagar
+ * @copyright   Magento Developers / magedeveloper.de <kontakt@magedeveloper.de>
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Editor extends Text
+{
+	/**
+	 * Initializes all form data providers to
+	 * $this->formDataProviders
+	 *
+	 * Will be executed in order of the added providers!
+	 *
+	 * @return void
+	 */
+	public function initializeFormDataProviders()
+	{
+		//$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class;
+		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseEffectivePid::class;
+		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaText::class;
+		parent::initializeFormDataProviders();
+	}
+
+	/**
+	 * Gets built tca array
+	 *
+	 * @return array
+	 */
+	public function buildTca()
+	{
+		$fieldName 					= $this->getField()->getUid()."_editor";
+		$tableName 					= "tx_dataviewer_domain_model_record";
+		$value 						= $this->getValue();
+		$databaseRow 				= $this->getDatabaseRow();
+		$databaseRow[$fieldName] 	= $value;
+
+		$tca = [
+			"tableName" => $tableName,
+			"databaseRow" => $databaseRow,
+			"fieldName" => $fieldName,
+			"effectivePid" => $this->getField()->getConfig("pid_config"),
+			"processedTca" => [
+				"columns" => [
+					$fieldName => [
+						"exclude" => (int)$this->getField()->isExclude(),
+						"label" => $this->getField()->getFrontendLabel(),
+						"config" => [
+							"type" => "text",
+							"renderType" => "t3editor",
+							"format" => $this->getField()->getConfig("format")?:"mixed",
+							"rows" => $this->getField()->getConfig("rows")?:"10",
+							"enableTabulator" => true,
+							"eval" => $this->getField()->getEval(),
+						],
+					],
+				],
+			],
+			"inlineStructure" => [],
+		];
+		
+		$this->prepareTca($tca);
+		$this->tca = $tca;
+		return $this->tca;
+	}
+
+}
