e6376e11251ffa62ed0fe1ae679665229e17920e
Bastian Zagar <zagar@aixdesign.net>
2017-05-20 20:32:57 +0200

    Singleton Instance

--- Classes/Factory/RecordFactory.php
+++ Classes/Factory/RecordFactory.php
@@ -18,7 +18,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * @copyright   Magento Developers / magedeveloper.de <kontakt@magedeveloper.de>
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class RecordFactory
+class RecordFactory implements SingletonInterface
 {
 	/**
 	 * Object Manager
