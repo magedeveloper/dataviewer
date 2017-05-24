aea3167c6713eb8e7435d4c4c2666be943276b8b
Bastian Zagar <zagar@aixdesign.net>
2017-05-24 17:00:16 +0200

    Version compatibilty

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
 	"CGLcompliance_note" => "",
 	"constraints" => [
 		"depends" => [
-			"typo3" => "8.7.0-8.7.99",
+			"typo3" => "8.7.0-8.7.1",
 			"extbase" => "",
 			"fluid" => "",
 		],
