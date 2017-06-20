cea3030c9eb96caf4800af555e80e729ee3b6b70
Bastian Zagar <zagar@aixdesign.net>
2017-06-20 19:22:10 +0200

    UserFunc for TypoScript Record/Fluid usage

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,4 +4,5 @@ $extPath  = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('datavie
 return [
 	'MageDeveloper\\Dataviewer\\Hooks\\ExtTablesInclusion' 		=> $extPath . 'Classes/Hooks/ExtTablesInclusion.php',
 	'MageDeveloper\\Dataviewer\\RealUrl\\RealUrlConfiguration'	=> $extPath . 'Classes/RealUrl/RealUrlConfiguration.php',
+	'MageDeveloper\\Dataviewer\\TypoScript\\UserFunc' 			=> $extPath . 'Classes/TypoScript/UserFunc.php',
 ];
